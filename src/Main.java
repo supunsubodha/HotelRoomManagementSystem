a212b8fc921ae9511fc66bb8edd19fb46eb9f73f
supunsubodha <supunmalsingha@gmail.com>
2025-03-23 11:37:20 +0530

    Implemented file reading and array population

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        
+        // Create an ArrayList to hold room numbers
+        ArrayList<String> allrooms = new ArrayList<>();
+        String filepath = "allrooms.txt";
+        try (BufferedReader br = new BufferedReader(new FileReader(filepath))) {
+            String line;
+
+            // Read each line from the file
+            while ((line = br.readLine()) != null) {
+                // Add each line to the ArrayList
+                allrooms.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Convert ArrayList to Array
+        String[] linesArray = new String[allrooms.size()];
+        allrooms.toArray(linesArray);
+        System.out.println("All rooms: " + allrooms);
     }
 }
